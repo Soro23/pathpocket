55888eb39d1337b560af304bf5242cf217d3dce3
Soro23 <aitor23.soro@gmail.com>
2023-11-21 18:44:26 +0100

    update to jquery

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
-const navItems = document.querySelectorAll(".nav-item");
-
-navItems.forEach((navItem, i) => {
-  navItem.addEventListener("click", () => {
-    navItems.forEach((item, j) => {
-      item.className = "nav-item";
-    });
-    navItem.className = "nav-item active";
-  });
-});
-
-
-
 $(document).ready(function(){
+  // Cargar la home al cargar la app
   $.load_page('home');
+
+  $('.navbar-nav').click(function(e) {
+    $('.navbar-nav.active').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+
 });
 
+// const navItems = document.querySelectorAll(".nav-item");
+
+// navItems.forEach((navItem, i) => {
+//   navItem.addEventListener("click", () => {
+//     navItems.forEach((item, j) => {
+//       item.className = "nav-item";
+//     });  
+//     navItem.className = "nav-item active";
+//   });  
+// });  
 
 
 
 
 
+/**
+ * Funciones generales
+ */
 $.load_page = function(pagename) {
   $("#content").load('page/'+pagename+'.html');
 }
