2de73340f624c6356f77313a87678c4f7ac86e04
Soro23 <aitor23.soro@gmail.com>
2023-11-21 14:21:05 +0100

    update css, and js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const navItems = document.querySelectorAll(".nav-item");
 
-navItems.forEach((navItem, i) => {
-  navItem.addEventListener("click", () => {
-    navItems.forEach((item, j) => {
-      item.className = "nav-item";
+console.log("entra");
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    const navItems = document.querySelectorAll(".nav-item");
+
+    navItems.forEach((navItem, i) => {
+        navItem.addEventListener("click", () => {
+            navItems.forEach((item, j) => {
+                item.className = "nav-item";
+            });
+            navItem.className = "nav-item active";
+        });
     });
-    navItem.className = "nav-item active";
-  });
 });
