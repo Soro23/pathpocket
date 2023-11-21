f5db31f79512aacd387e9fab95b50824518e2037
Soro23 <aitor23.soro@gmail.com>
2023-11-21 16:33:58 +0100

    Dynamic test

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", (event) => {
+  load_page('home');
+});
+
 const navItems = document.querySelectorAll(".nav-item");
 
 navItems.forEach((navItem, i) => {
@@ -8,3 +12,8 @@ navItems.forEach((navItem, i) => {
     navItem.className = "nav-item active";
   });
 });
+
+
+function load_page(pagename) {
+  document.getElementById("content").innerHTML='<object type="text/html" data="page/'+pagename+'.html"></object>';
+}
