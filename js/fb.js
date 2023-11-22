db010c406b49ad3bb1aabc11500698b079d8137a
Soro23 <aitor23.soro@gmail.com>
2023-11-22 11:23:00 +0100

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -1,25 +1,22 @@
 import * as pathfinder from "./pathfinderdb.js";
 // import "./pathfinderdb.js";
 
-
 // Ejemplo de escritura en la base de datos
 var data = {
-  Patrick: {
-    stats: {
-      fue: 16,
-    },
+  name: "Patrick",
+  stats: {
+    fue: 16,
   },
 };
 
 // Establece datos en un nodo específico
-pathfinder.DB.getDB().ref("characters").push(data);
+pathfinder.DB.writeCharacterData(data);
 
-// Ejemplo de lectura de la base de datos
-pathfinder.DB.getDB()
-  .ref("characters")
-  .once("Patrick")
-  .then(function (snapshot) {
-    var result = snapshot.val();
-    console.log(result);
-  });
-
+// // Ejemplo de lectura de la base de datos
+// pathfinder.DB.getDB()
+//   .ref("characters")
+//   .once("Patrick")
+//   .then(function (snapshot) {
+//     var result = snapshot.val();
+//     console.log(result);
+//   });
