3a0053bd8d87075a06f015d010bfafcb40b8ff67
Soro23 <aitor23.soro@gmail.com>
2023-11-24 14:04:58 +0100

    update menu

--- sao/js/menu.js
+++ sao/js/menu.js
@@ -33,19 +33,18 @@ $(document).ready(function () {
    * SUBMENU FUNCTIONALLITY
    */
   $(document).on("click", function (e) {
-    if (
-      $(e.target).is($("#nav-options")) ||
-      $.contains($("#nav-options")[0], e.target)
-    ) {
-      //Se clicka en el menu
-      $("#nav-options li").click(function (e) {
-        $("#nav-options li").removeClass("active");
-        $(this).toggleClass("active");
-		$('#nav-options > li.active ul.floating.submenu0').css('height', $('#nav-options > li.active ul.floating.submenu0').children().length* 60)
-      });
-    } else {
-      //Se clica fuera
+    if (!$(e.target).is($("#nav-options")) || !$.contains($("#nav-options")[0], e.target)) {
+      //Se clica fuera del menu
       $("#nav-options li.active").removeClass("active");
+      $('#submenu0counter').remove();
+    }
+  });
+  $("#nav-options li").click(function (e) {
+    $('#submenu0counter').remove();
+    $("#nav-options li.active").toggleClass('active');
+      $(e.currentTarget).addClass("active");
+      if (!$('#submenu0counter').length) {
+        $('<style id="submenu0counter">#nav-options > li.active ul.floating.submenu0 { height: ' + $('#nav-options > li.active ul.floating.submenu0').children().length * 60 + '; }</style>').appendTo('body');
     }
   });
 });
