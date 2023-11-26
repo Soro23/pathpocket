0dbe07814e539b32178c812164c156182dbb6fb2
Soro23 <aitor23.soro@gmail.com>
2023-11-26 17:48:09 +0100

    a

--- sao/js/main.js
+++ sao/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-  console.log(navigator.userAgent);
   // Bloquear el uso de web 10 segundos
-  setTimeout(function () {
-    $('css-doodle').css('box-shadow', 'inset 0px 0px 200px -200px #000');
-    loadJS("js/menu.js", true);
-    loadJS("js/modal-append.js", true);
-    loadJS("js/modal.js", true);
-  }, 5000);
+  // setTimeout(function () {
+  // $('css-doodle').css('box-shadow', 'inset 0px 0px 200px -200px #000');
+  $('css-doodle').css('width', '0').css('height', '0');
+  loadJS("js/menu.js", true);
+  loadJS("js/modal-append.js", true);
+  loadJS("js/modal.js", true);
+  // }, 5000);
 
 
 });
@@ -25,7 +25,7 @@ function loadJS(FILE_URL, async = true) {
   scriptEle.addEventListener("load", () => {
     console.log("File loaded")
   });
-   // error event
+  // error event
   scriptEle.addEventListener("error", (ev) => {
     console.log("Error on loading file", ev);
   });
