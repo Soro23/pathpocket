ecee4a7a45adbb2344ed47c6e2c85ec494b5898b
Soro23 <aitor23.soro@gmail.com>
2023-12-18 21:43:07 +0100

    nextjs base

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',   
+    images: {
+        unoptimized: true,
+    },
+  }
+   
+  module.exports = nextConfig
