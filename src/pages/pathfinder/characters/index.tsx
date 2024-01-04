f55c478bd40ff51dbd6cd9fc0012b5483d1ce561
Aitor IDGL <aitor23.soro@gmail.com>
2024-01-04 17:13:24 +0100

    update characterlist

--- src/pages/pathfinder/characters/index.tsx
+++ src/pages/pathfinder/characters/index.tsx
@@ -1,19 +1,44 @@
-import type { NextPage } from 'next'
-import { Box, Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, SimpleGrid, Stack, Image, Text, Center, IconButton, useColorModeValue, Flex, Spacer, color } from '@chakra-ui/react';
+import type { NextPage } from "next";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Image,
+  Text,
+  Center,
+  IconButton,
+  useColorModeValue,
+  Flex,
+  Spacer,
+  color,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useAuth } from "@/contexts/AuthUserContext";
 import { getUserCharacters } from "@/services/firebase/database";
-import CharacterAvatarEditor from '@/components/ui/molecules/CharacterAvatarEditor';
-import { Button as CButton } from '@/components/ui/atoms/Button';
-import { RiAddFill } from 'react-icons/ri';
-import { useRouter } from 'next/router';
-
-
+import CharacterAvatarEditor from "@/components/ui/molecules/CharacterAvatarEditor";
+import { CharacterData } from "@/components/class/characterdata";
+import { Button as CButton } from "@/components/ui/atoms/Button";
+import { RiAddFill } from "react-icons/ri";
+import { useRouter } from "next/router";
 
 const Characters: NextPage = () => {
   const { authUser } = useAuth();
   const [characters, setCharacters] = useState<any[]>([]);
   const router = useRouter();
+
+  const gColumns = useBreakpointValue({
+    base: 2,
+    md: 3,
+    lg: 5,
+  });
 
   useEffect(() => {
     if (authUser) {
@@ -22,7 +47,21 @@ const Characters: NextPage = () => {
           if (Array.isArray(characters)) {
             setCharacters(characters);
           } else {
-            const charactersArray = Object.entries(characters).map(([key, val]) => ({ key, val }));
+            // const charactersArray = Object.entries(characters).map(
+            //   ([key, val]) => ({ key, val })
+            // );
+
+            // Creas un nuevo array de objetos CharacterData utilizando Object.entries y el método map.
+            const charactersArray = Object.entries(characters).map(
+              // Para cada entrada (key, val) del objeto 'characters', se crea un nuevo objeto CharacterData.
+              ([key, val]) => {
+                let oldval = val;
+                val = new CharacterData();
+                // Se utiliza el método copyFrom para copiar las propiedades de 'val' al nuevo objeto CharacterData.
+                val.copyFrom(oldval as CharacterData | undefined);
+                return { key, val };
+              }
+            );
             setCharacters(charactersArray);
           }
         })
@@ -33,46 +72,57 @@ const Characters: NextPage = () => {
     }
   }, [authUser]);
 
-  const headingColor = useColorModeValue('maroon', 'wheat')
+  const headingColor = useColorModeValue("maroon", "wheat");
 
   const navigateToEditPage = (characterId: string) => {
     router.push(`/pathfinder/characters/${characterId}`);
   };
+  const logthis = (log: any) => {
+    console.log(log);
+  };
 
   return (
     <Box w="full" p={4}>
-      <Flex minWidth='max-content' alignItems='center' gap='2' p={4}>
-        <Box p='2'>
-          <Heading as='h3' size='lg' style={{ color: headingColor }}>Personajes</Heading>
+      <Flex minWidth="max-content" alignItems="center" gap="2" p={4}>
+        <Box p="2">
+          <Heading as="h3" size="lg" style={{ color: headingColor }}>
+            Personajes
+          </Heading>
         </Box>
         <Spacer />
-        <ButtonGroup gap='2'>
-          <CButton cvariant={true} rightIcon={<RiAddFill />}>Nuevo</CButton>
+        <ButtonGroup gap="2">
+          <CButton cvariant={true} rightIcon={<RiAddFill />}>
+            Nuevo
+          </CButton>
         </ButtonGroup>
       </Flex>
-      <SimpleGrid columns={5} spacing={10}>
+      <SimpleGrid columns={gColumns} spacing={5}>
         {characters.map((character) => (
-          <Card key={character.key} maxW='sm'>
+          <Card key={character.key} maxW="sm">
             <CardBody>
               <Image
-                src={character.val.imagesrc ?? 'https://firebasestorage.googleapis.com/v0/b/soro-dashboard.appspot.com/o/users%2FdE3IicCMypbQNL0ojqIBdDGXdxE3%2Fpublic%2Fcharacters%2FT3?alt=media&token=914e6c5d-c018-488a-8b10-ce76f6f0cae3'}
+                src={character.val.imagesrc}
                 alt={character.val.name}
-                borderRadius='lg'
+                borderRadius="lg"
               />
-              <Stack mt='6' spacing='3'>
-                <Heading size='md'><Center>{character.val.name}</Center></Heading>
+              <Stack mt="6" spacing="3">
+                <Heading size="md" onClick={() => logthis(character)}>
+                  <Center>{character.val.name}</Center>
+                </Heading>
               </Stack>
             </CardBody>
             <Divider />
-            <CardFooter minWidth='max-content' justify={'space-between'}>
-              <CButton onClick={() => navigateToEditPage(character.key)}>Edit</CButton>
+            <CardFooter minWidth="max-content" justify={"space-between"}>
+              <CButton onClick={() => navigateToEditPage(character.key)}>
+                Edit
+              </CButton>
               <CharacterAvatarEditor name={character.val.name} />
             </CardFooter>
           </Card>
         ))}
       </SimpleGrid>
-    </Box >
+    </Box>
   );
-}
+};
 
 export default Characters;
