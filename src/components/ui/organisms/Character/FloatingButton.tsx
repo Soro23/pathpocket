fe210fec8d386b1eb7b22a756177295e8d3dff9c
Soro23 <aitor23.soro@gmail.com>
2024-01-09 12:04:04 +0100

    Add floatingmodes

---
+++ src/components/ui/organisms/Character/FloatingButton.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import { Box, Button, HStack, useColorModeValue } from '@chakra-ui/react';
+import { Fade } from 'react-awesome-reveal';
+import { GiAxeSword, GiPapers, GiPencil, GiAtomicSlashes } from 'react-icons/gi';
+
+export const FloatingButton = () => {
+    const [showOptions, setShowOptions] = useState(false);
+    const headingColor = useColorModeValue('maroon', 'wheat')
+    const borderColor = useColorModeValue('wheat','maroon')
+    const textColor = useColorModeValue('white', 'black')
+
+    const toggleOptions = () => {
+        setShowOptions(!showOptions);
+    };
+
+    return (
+        <HStack
+            position="fixed"
+            bottom="50px"
+            right="50px"
+            spacing={4}
+            align="flex-end"
+        >
+            {showOptions ? (
+                <>
+                    <Button
+                        w="75px"
+                        h="75px"
+                        position="absolute"
+                        bottom="25px"
+                        right="-12px"
+                        borderRadius="100% 100% 0 0"
+                        style={{
+                            clipPath: 'polygon(0 0, 100% 0, 50% 100%, 50% 100%)',
+                            transform: 'rotate(0deg)',
+                            transformOrigin: 'bottom',
+                            alignItems: 'start',
+                            paddingTop: '20px'
+                        }}
+                        backgroundColor={headingColor}
+                    >
+                        <GiAxeSword style={{ color: textColor }} />
+
+                    </Button>
+                    <Button
+
+                        w="75px"
+                        h="75px"
+                        backgroundColor={headingColor}
+                        // onClick={toggleOptions}
+                        position="absolute"
+                        bottom="25px"
+                        right="-12px"
+                        borderRadius="100% 100% 0 0"
+                        style={{
+                            clipPath: 'polygon(0 0, 100% 0, 50% 100%, 50% 100%)',
+                            transform: 'rotate(305deg)',
+                            transformOrigin: 'bottom',
+                            alignItems: 'start',
+                            paddingTop: '20px'
+                        }}
+                    >
+                        <GiPapers style={{ color: textColor }} />
+                    </Button>
+                    <Button
+                        w="75px"
+                        h="75px"
+                        backgroundColor={headingColor}
+                        position="absolute"
+                        bottom="25px"
+                        right="-12px"
+                        borderRadius="100% 100% 0 0"
+                        style={{
+                            clipPath: 'polygon(0 0, 100% 0, 50% 100%, 50% 100%)',
+                            transform: 'rotate(250deg)',
+                            transformOrigin: 'bottom',
+                            alignItems: 'start',
+                            paddingTop: '20px'
+                        }}
+                    >
+                        <GiPencil style={{ color: textColor }} />
+                    </Button>
+                </>
+            ) : null}
+
+            <Button
+                colorScheme="fill"
+                borderRadius="full"
+                onClick={toggleOptions}
+                backgroundColor={headingColor}
+                h="50px"
+                w="50px"
+                border="2px solid"
+                borderColor={textColor}
+            >
+                <GiAtomicSlashes style={{ color: textColor }} />
+            </Button>
+        </HStack >
+    );
+};
--- src/pages/pathfinder/characters/new/index.tsx
+++ src/pages/pathfinder/characters/new/index.tsx
@@ -4,9 +4,12 @@ import { Box, Spinner } from '@chakra-ui/react'
 import { useAuth } from 'contexts/AuthUserContext';
 import { SoroLogo } from '@/utils/soro-logo';
 import { useEffect } from 'react';
+import { CharacterData } from "@/components/class/characterdata";
+import { TopHeader } from '@/components/ui/organisms/Character/TopHeader';
 
 const NewCharacter: NextPage = () => {
     const { loading: loadingAuth, authUser } = useAuth()
+    let newChar = new CharacterData()
 
     useEffect(() => {
         if (authUser) {
@@ -15,14 +18,12 @@ const NewCharacter: NextPage = () => {
     }, [authUser]);
 
     return (
-        <Flex
-            align="center"
-            justify="center"
-        >
-            {loadingAuth && <Spinner />}
-            <Text>{authUser?.uid}</Text>
-            <SoroLogo size={50} />
-        </Flex>
+        <Box w="full" h="full" >
+            {/* {loadingAuth && <Spinner />} */}
+            {/* <Text>{authUser?.uid}</Text> */}
+            {/* <SoroLogo size={50} /> */}
+            <TopHeader CharData={newChar} editMode={true} />
+        </Box>
 
     )
 }
