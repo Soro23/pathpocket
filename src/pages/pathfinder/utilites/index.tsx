8a5452b40ebfc08afda543bd2e8831413da7c1b1
Aitor IDGL <aitor23.soro@gmail.com>
2024-01-24 12:56:19 +0100

    Update index.tsx

--- src/pages/pathfinder/utilites/index.tsx
+++ src/pages/pathfinder/utilites/index.tsx
@@ -1,12 +1,46 @@
-import type { NextPage } from 'next'
-import { Box } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import {
+  Box,
+  Flex,
+  Heading,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+  Spacer,
+  useColorModeValue,
+} from '@chakra-ui/react';
+import { GiCheckMark, GiSoundOn } from 'react-icons/gi';
 
 const Utilities: NextPage = () => {
+  const headingColor = useColorModeValue('maroon', 'wheat');
   return (
-    <Box w="full">
-      Utilities
+    <Box w="full" p={4}>
+      <Flex minWidth="max-content" alignItems="center" gap="2" p={4}>
+        <Box p="2">
+          <Heading as="h3" size="lg" style={{ color: headingColor }}>
+            Utilities
+          </Heading>
+        </Box>
+        <Spacer />
+      </Flex>
+
+      <List spacing={3}>
+        <ListItem>
+          <ListIcon as={GiCheckMark} color="green.500" />
+          <Link href="https://www.d20pfsrd.com/" isExternal>
+            d29pfsrd
+          </Link>
+        </ListItem>
+        <ListItem>
+          <ListIcon as={GiSoundOn} color="green.500" />
+          <Link href="https://www.pocketbard.app/" isExternal>
+            Pocket Bard
+          </Link>
+        </ListItem>
+      </List>
     </Box>
-  )
-}
+  );
+};
 
 export default Utilities;
